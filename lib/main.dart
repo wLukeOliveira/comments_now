52352e26bd4cdf84cce4046e1e01297c9dfeded8
Arthur Lamberti <39394861+ArthurLamberti@users.noreply.github.com>
2023-03-13 20:53:05 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<void> _startCountdown() async {
+  Future<void> _startCountdown() async {s
     setState(() {
       _countdown = _textController.text;
     });
